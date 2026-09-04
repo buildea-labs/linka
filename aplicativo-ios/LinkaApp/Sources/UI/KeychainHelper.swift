b0c4427e1cfc16632d2f508440f9690c2826827a
Giammattey - MacBook Air <giammattey.luiz@gmail.com>
2026-09-04 13:48:20 -0300

    feat: adicionar acesso ao roteador com GatewayScanner e WebView interna

---
+++ aplicativo-ios/LinkaApp/Sources/UI/KeychainHelper.swift
@@ -0,0 +1,58 @@
+import Foundation
+import Security
+
+class KeychainHelper {
+    static let shared = KeychainHelper()
+    
+    private init() {}
+    
+    func save(_ data: Data, service: String, account: String) {
+        let query = [
+            kSecValueData: data,
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrService: service,
+            kSecAttrAccount: account,
+        ] as CFDictionary
+        
+        let status = SecItemAdd(query, nil)
+        
+        if status == errSecDuplicateItem {
+            let query = [
+                kSecAttrService: service,
+                kSecAttrAccount: account,
+                kSecClass: kSecClassGenericPassword,
+            ] as CFDictionary
+            
+            let attributesToUpdate = [kSecValueData: data] as CFDictionary
+            SecItemUpdate(query, attributesToUpdate)
+        }
+    }
+    
+    func read(service: String, account: String) -> Data? {
+        let query = [
+            kSecAttrService: service,
+            kSecAttrAccount: account,
+            kSecClass: kSecClassGenericPassword,
+            kSecReturnData: true
+        ] as CFDictionary
+        
+        var item: CFTypeRef?
+        let status = SecItemCopyMatching(query, &item)
+        
+        if status == errSecSuccess {
+            return (item as? Data)
+        } else {
+            return nil
+        }
+    }
+    
+    func delete(service: String, account: String) {
+        let query = [
+            kSecAttrService: service,
+            kSecAttrAccount: account,
+            kSecClass: kSecClassGenericPassword
+        ] as CFDictionary
+        
+        SecItemDelete(query)
+    }
+}
