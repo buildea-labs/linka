290804f99d31152965a63657d04444bf8864548f
Giammattey - MacBook Air <giammattey.luiz@gmail.com>
2026-09-20 19:19:28 -0300

    feat(web): adicionar paginas de privacidade e suporte de casawifi e lagcheck

--- aplicacao-web/src/App.tsx
+++ aplicacao-web/src/App.tsx
@@ -2,13 +2,17 @@ import { LandingScreen } from './screens/LandingScreen';
 import { InfoScreen } from './screens/InfoScreen';
 import { NotFoundScreen } from './screens/NotFoundScreen';
 
-const infoRoutes = new Set(['/sobre', '/como-medimos', '/privacidade', '/termos', '/suporte']);
+const infoRoutes = new Set(['/sobre', '/como-medimos', '/privacidade', '/termos', '/suporte', '/casawifi/privacidade']);
 
 export default function App() {
   const pathname = window.location.pathname.replace(/\/$/, '') || '/';
 
   if (pathname === '/') {
     return <LandingScreen />;
+  }
+
+  if (pathname === '/casawifi' || pathname === '/casawifi/privacidade') {
+    return <InfoScreen pathname="/casawifi/privacidade" />;
   }
 
   if (infoRoutes.has(pathname)) {
